e5b7fd5abec42f9ae1f0c657f7a440f6ff7e53e2
DimitrenkoTaras <dimitrenkotaras@gmail.com>
2020-12-23 16:48:52 +0200

    version-final

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+    $('.header__list').click(function(event) {
+        $('.header__burger, .header__menu').removeClass('active');
+        $('body').removeClass('lock');
+    });
+
+    $('.slider-events').slick({
+        speed:1500,
+        dots:true,
+        arrows:false,      
+    });
+});
